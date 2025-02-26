b95e63baba9116bf2f4120b42efbccb88697ef7b
nxt5656 <nxt5656@live.cn>
2025-02-26 01:47:43 +0000

    jupyterlab启动时,使用配置文件,添加插件中文支持

--- 0-学习环境的搭建/build/jupyter_lab_config.py
+++ 0-学习环境的搭建/build/jupyter_lab_config.py
@@ -1,8 +1,6 @@
-
 c.NotebookApp.allow_root = True
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.token = 'UserPas$0w'
 c.NotebookApp.terminado_settings = {"shell_command": ["/bin/bash"]}
-c.LanguageServerManager.language = 'zh-CN'
